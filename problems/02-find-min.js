c6831d245776d04c0509ac5089379c46ead64a33
Jeff <jeffantoine19.ja@gmail.com>
2021-05-25 16:19:42 -0500

    finished findMin and getHeight methods

--- problems/02-find-min.js
+++ problems/02-find-min.js
@@ -3,7 +3,10 @@
 // binary search tree and returns the minimum value.
 // You may use your BST implementation to test this out.
 function findMin(root) {
-    // Your code here
+  if (!root) return null
+  
+  while (root.left) root = root.left
+  return root
 }
 
 
--- problems/03-get-height.js
+++ problems/03-get-height.js
@@ -2,8 +2,16 @@
 // binary search tree and returns the height.
 // You may use your BST implementation to test this out.
 function getHeight(root) {
+  if (!root) return - 1
+  
+  let right = getHeight(root.right)
+  let left = getHeight(root.left)
 
+  if (left > right) {
+    return left + 1
+  }else return right + 1
 }
+
 
 
 module.exports = {
